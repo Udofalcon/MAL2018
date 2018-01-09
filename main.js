8f4ed9e3f68ec0439f399a99126e5b335212e2bd
Udofalcon <Bear.Arms.Cavalry@gmail.com>
2018-01-08 22:15:11 -0500

    Update main.js

--- main.js
+++ main.js
@@ -46,18 +46,42 @@ var difficulties = [
 ];
 var maxHeight = 108;
 var challenges = [];
+var days = [];
+var dateCalc = new Date(2018, 0, 1);
+
+do {
+  days.push(dateCalc.getTime());
+  dateCalc.setDate(dateCalc.getDate() + 1);
+} while (dateCalc.getFullYear() !== 2019);
 
 canvas.width = document.body.width;
 canvas.height = document.body.height;
 
 for (var i = 0; i < difficulties.length; i++) {
-  difficulties.heightData = [];
+  difficulties.pointData = [];
   maxHeight = Math.max(maxHeight, difficulties[i].total);
   
-  for (var j = 0; j <= canvas.width; j++) {
-    difficulties[i].heightData[j] = (j / canvas.width) * (difficulties[i].total / maxHeight) * canvas.height;
+  for (var j = 0; j < days.length; j++) {
+    difficulties[i].pointData[j] = {
+      date: days[j],
+      x: j / (days.length - 1),
+      y: (j / (days.length - 1)) * (difficulties[i].total / maxHeight)
+    };
   }
 }
 
+for (var i = 0/*difficulties.length - 1*/; i >= 0; i--) {
+  ctx.fillStyle = 'rgb(' + parseInt((i + 1) / difficulties.length * 255) + ', 0, 0)';
+  ctx.moveTo(difficulties[i].pointData[0].x, difficulties[i].pointData[0].y);
+  
+  for (var j = 1; j < difficulties.pointData.length; j++) {
+    ctx.lineTo(difficulties[i].pointData[j].x, difficulties[i].pointData[j].y);
+  }
+  
+  ctx.closePath();
+  ctx.fill();
+}
+
 // 1/8
-// challenges += 6/6; 1. Time of Eve
+// 1. Time of Eve (6/6)
+//addEntry(6/6, Date(2018, 0, 8));
